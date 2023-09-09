16d38b1d1f51437b07bf0cd146da398b46bf813e
Coding4fun2012 <ljmasdf110@gmail.com>
2023-09-09 17:46:12 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("isNiao", 1)
+    radio.sendString("A")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
 })
 radio.setGroup(1)
 basic.forever(function () {
-	
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P0))
+    if (0 == pins.digitalReadPin(DigitalPin.P0)) {
+        radio.sendString("B")
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
 })
